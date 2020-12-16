8049549c029932b855b696ba797cd4c76856ab13
Krzysztof Jajeśnica <krzysztof.jajesnica@gmail.com>
2020-12-17 00:09:34 +0100

    Changed ContentRegistry.register* to private, added comments.

--- app/src/main/java/ubh/loader/ContentLoader.java
+++ app/src/main/java/ubh/loader/ContentLoader.java
@@ -2,7 +2,22 @@ package ubh.loader;
 
 import org.hjson.JsonValue;
 
+/** {@code ContentLoader} is responsible for loading a single type of content from a JSON description.
+ * <p>
+ * 
+ *
+ * @param <T> Type of the loaded content.
+ */
 @FunctionalInterface
 public interface ContentLoader<T> {
-	public T load(ContentRegistry registry, JsonValue data) throws ContentException;
+	/** Loads a piece of content.
+	 * @implNote
+	 * {@code load()} should not perform type checking / error checking on the passed {@code json}; it is ok if {@code load()}
+	 * fails with an exception. It is the responsibility of the {@link ContentRegistry} using the loader to recover from the error.
+	 * @param registry Registry used to load dependencies for the content.
+	 * @param json Describes the content.
+	 * @return The loaded content.
+	 * @throws ContentException When loading the content fails. 
+	 */
+	public T load(ContentRegistry registry, JsonValue json) throws ContentException;
 }
--- app/src/main/java/ubh/loader/ContentRegistry.java
+++ app/src/main/java/ubh/loader/ContentRegistry.java
@@ -12,35 +12,86 @@ import ubh.attack.*;
 import ubh.entity.*;
 import ubh.math.*;
 
+/** {@code ContentRegistry} is responsible for loading various types of game content. */
 public final class ContentRegistry {
 	
-	private Map<String, ContentLoader<?>> loaders = new HashMap<>();
+	private ContentRegistry() {}
+	
+	private Map<String, ContentLoader<?>> namedLoaders = new HashMap<>();
 	private Map<Class<?>, ContentLoader<?>> defaultLoaders = new HashMap<>();
 	private Map<Class<?>, Object> defaultValues = new HashMap<>();
 	
-	public void registerLoader(String name, ContentLoader<?> loader) {
-		loaders.put(name, loader);
+	/** Registers a named content loader.
+	 * <p>
+	 *  This loader will be dispatched on {@code JsonObject} instances with a {@code type}
+	 *  property matching {@code name} used to register the loader.
+	 * @param name 
+	 * @param loader
+	 * @see ContentRegistry#load
+	 */
+	private void registerLoader(String name, ContentLoader<?> loader) {
+		namedLoaders.put(name, loader);
 	}
-	public <T> void registerLoader(Class<T> clazz, ContentLoader<T> loader) {
+	/** Registers a default content loader for a content type. 
+	 *  <p>
+	 *  Default content loader serves as a fallback when named loader does not exist, fails, or can not be found
+	 *  due to missing {@code type} property on the {@code JsonValue}.
+	 *  <p>
+	 *  Default content loader is dispatched based on the expected content type.
+	 * @param <T> Type of the loaded content.
+	 * @param clazz Should be {@code T.class}, this is needed because Java uses type erasure for generics. 
+	 * @param loader
+	 * @see ContentRegistry#load
+	 */
+	private <T> void registerLoader(Class<T> clazz, ContentLoader<T> loader) {
 		defaultLoaders.put(clazz, loader);
 	}
-	public <T> void registerDefault(Class<T> clazz, T value) {
+	/** Registers a default value for a content type.
+	 *  <p>
+	 *  The default value serves as a final fallback when both named and default loader for the content are missing or fail.
+	 *  
+	 * @param <T> Type of the content.
+	 * @param clazz Should be {@code T.class}, this is needed because Java uses type erasure for generics. 
+	 * @param value The default value for the content type. 
+	 * <p> 
+	 * Default value should be safe to use as a replacement for loaded content;
+	 * it may miss functionality, but must not crash the program or cause unexpected behavior (for this reason {@code null} and NaN are
+	 * not valid default values).
+	 * <p>
+	 * Default value should be immutable, as it may be shared by multiple objects.
+	 */
+	private <T> void registerDefault(Class<T> clazz, T value) {
 		defaultValues.put(clazz, value);
 	}
 	
+	/** Loads content from passed JsonValue.
+	 *  <p>
+	 *  If {@code JsonValue} has a {@code type} property, then its value is used to dispatch a named content loader.
+	 *  <p>
+	 *  If there is no {@code type} property, or the named loader is missing, or it fails, then loading is attempted using
+	 *  the default loader for the expected content type ({@code T})
+	 *  <p>
+	 *  If both of the above fail, then the registered default value for the expected content type is returned.
+	 * 
+	 * @param <T> Expected type of the loaded content.
+	 * @param clazz Should be {@code T.class}, this is needed because Java uses type erasure for generics. 
+	 * @param json JsonValue describing the loaded content.
+	 * @return The loaded content.
+	 * @throws ContentException If all possible loaders fail and no default value is registered for the content type.
+	 */
 	@SuppressWarnings("unchecked")
-	public <T> T load(Class<T> clazz, JsonValue data) throws ContentException {
+	public <T> T load(Class<T> clazz, JsonValue json) throws ContentException {
 		
 		Object result = null;
 		
-		// (H)Json objects should have a "type" property describing which loader to use
-		if(data.isObject()) {
-			final var obj = data.asObject();
+		// Try to use named loader
+		if(json.isObject()) {
+			final var obj = json.asObject();
 			final var type = obj.getString("type", null);
 			if(type != null) {
-				final var loader = loaders.get(type);
+				final var loader = namedLoaders.get(type);
 				if(loader != null) {
-					try {result = loader.load(this, data);}
+					try {result = loader.load(this, json);}
 					catch (Exception e) {
 						// TODO: THIS IS HORRIBLE
 						// REPLACE IT WITH PROPER EXCEPTION HANDLING LATER
@@ -51,11 +102,11 @@ public final class ContentRegistry {
 				}
 			}
 		}
-		// Try to use default loader for specified type
+		// Try to use default loader
 		if(result == null) {
 			final var loader = defaultLoaders.get(clazz);
 			if(loader != null)
-				try {result = loader.load(this, data);}
+				try {result = loader.load(this, json);}
 				catch (Exception e) {
 					// TODO: THIS IS HORRIBLE
 					// REPLACE IT WITH PROPER EXCEPTION HANDLING LATER
@@ -69,24 +120,37 @@ public final class ContentRegistry {
 		if(result != null)
 			return (T) result;
 		else
+			// Everything failed, bail out
 			throw new ContentException("Failed to load content.");
 	}
 	
-	public <T> T loadFromResource(Class<T> loadedClass, Class<?> c, String resourceName) {
-		try(var reader = new InputStreamReader(c.getResourceAsStream(resourceName))) {
-			return load(loadedClass, JsonValue.readHjson(reader));
+	/** Loads content from a Hjson resource file. 
+	 * @param <T> Expected type of the loaded content.
+	 * @param clazz Should be {@code T.class}, this is needed because Java uses type erasure for generics. 
+	 * @param relativeTo Class used to load the resource file.
+	 * @param resourceName Name of the resource file.
+	 * @return The loaded content.
+	 */
+	public <T> T loadFromResource(Class<T> clazz, Class<?> relativeTo, String resourceName) {
+		try(var reader = new InputStreamReader(relativeTo.getResourceAsStream(resourceName))) {
+			return load(clazz, JsonValue.readHjson(reader));
 		} catch (IOException e) {
 			throw new Error(e);
 		}
 	}
 	
+	/** Creates a default {@code ContentRegistry} for UBH.
+	 *  This is where loaders and default values should be registered.
+	 */
 	public static ContentRegistry createDefault() {
 		var registry = new ContentRegistry();
 		
+		// JVM classes
 		registry.registerLoader(Color.class, (reg,json) -> {
 			if(json.isObject()) {
+				// Try to handle both RGB and HSV colors
 				boolean isHsv = false;
-				float r=0,g=0,b=0,h=0,s=0,v=0,a=1;
+				float r=0,g=0,b=0,h=0,s=1,v=1,a=1;
 				for(var member : json.asObject()) {
 					switch(member.getName()) {
 					case "r": case "red": r = member.getValue().asFloat(); break;
@@ -106,6 +170,7 @@ public final class ContentRegistry {
 					return new Color(r,g,b,a);
 				}
 			} else if(json.isString()) {
+				// Not using Color.getColor because I think it may not be portable
 				switch(json.asString().toLowerCase()) {
 				case "red": return Color.red;
 				case "green": return Color.green;
@@ -121,11 +186,13 @@ public final class ContentRegistry {
 				throw new ContentException(json.toString() + " is not a valid Color");
 			}
 		});
+		// ubh.math
 		registry.registerLoader(Vector2.class, Vector2::fromJson);
 		registry.registerLoader(Shape.class, Shape::fromJson);
 		registry.registerLoader("Circle", Circle::fromJson);
 		registry.registerLoader("Rectangle", Rectangle::fromJson);
 		
+		// ubh.attack, ubh.entity
 		registry.registerDefault(Attack.class, Attack.NULL);
 		registry.registerLoader("Bullet", Bullet::fromJson);
 		registry.registerLoader("Ship", Ship::fromJson);
